d4cdc3e12a261422fd042294d6d8a171e087b641
Abdellah Ramadan <ajrmdn@gmail.com>
2019-06-14 10:25:04 -0700

    study the .match method on w3schools to make the backend logic execute properly

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,20 @@
+function numberToOutput(string) {
+  var result = [];
+  for (var i = 1; i <= input; i++) {
 
+    if (i % 3 == 0) {
+      results.push
+    }
+  }
+}
 
 //------------- Front End Logic ---------//
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
    event.preventDefault();
-   var number = parseInt($("#number").val());
+   var number = parseInt($("input#number").val());
+   var result = numberToOutput(string);
+   $("#result").text(result);
  });
 });
 
