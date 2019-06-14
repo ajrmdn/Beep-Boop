a51f7ffe370d1c49a8ed66b8341769b747dc4ab4
Abdellah Ramadan <ajrmdn@gmail.com>
2019-06-14 11:24:26 -0700

    still work on debugging the if/else statements

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,30 @@
-function numberToOutput(string) {
+//----------------Back End Logic -----------//
+function numberToOutput(input) {
   var result = [];
   for (var i = 1; i <= input; i++) {
 
-    if (i % 3 == 0) {
-      result.push("I'm sorry, Dave. I'm afraid I can't do that.");
-    }
-    else if (i.toString().match(/0/)) {
-      result.push("Beep!")
-    }
-    else if (i.toString().match(/1/)) {
-      result.push("Boop!")
-    }else {
-     result.push(i)
-    }
-  }
+  //   if (i % 3 == 0) {
+  //     result.push("I'm sorry, Dave. I'm afraid I can't do that.");
+  //   }
+  //   else if (i.toString().match(/0/)) {
+  //     result.push("Beep!")
+  //   }
+  //   else if (i.toString().match(/1/)) {
+  //     result.push("Boop!")
+  //   }else {
+  //    result.push(i)
+  //   }
+  // }
+  // return result;
+}
 }
 
 //------------- Front End Logic ---------//
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
-   var number = parseInt($("input#number").val());
-   var result = numberToOutput(string);
+   var number = parseInt($("#number").val());
+   var result = numberToOutput(input);
    $("#result").text(result);
  });
 });
