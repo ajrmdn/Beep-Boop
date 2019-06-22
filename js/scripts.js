61fc23dbd2774887d14e7bb99aa8882483406f8f
Abdellah Ramadan <ajrmdn@gmail.com>
2019-06-22 12:30:41 -0700

    Add changes to my code to get an input result.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 //----------------Back End Logic -----------//
 function numberToOutput(number) {
   var result = [];
-  for (var i = 1; i <= number; i++) {
+  for (var i = 0; i <= number; i++) {
 
 
-    if (i.toString().match(/?/)) {
+    if (i.toString().match(/1/)) {
         result.push("Beep!")
     }
-    else if (i.toString().match(/?/)) {
+    else if (i.toString().match(/2/)) {
       result.push("Boop!")
     }
-    else if (i % 3 === ?) {
+    else if (i % 3 === 3) {
       result.push("I'm sorry, Dave. I'm afraid I can't do that.");
     }else {
      result.push(i)
@@ -25,7 +25,7 @@ $(document).ready(function() {
   $("#formOne").submit(function(event) {
     event.preventDefault();
    var number = parseInt($("#number").val());
-   var result = numberToOutput(input);
+   var result = numberToOutput(number);
    $("#result").text(result);
  });
 });
